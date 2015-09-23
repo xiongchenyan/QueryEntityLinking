9dbee80fa06c076becb5cbf8b0466e58fd1aaf99
chenyanxiong <xiongchenyan@gmail.com>
2015-09-22 20:27:36 -0400

    merge surface form

---
+++ SurfaceForm/SimpleMergeMulSurfaceFormFile.py
@@ -0,0 +1,53 @@
+'''
+Created on Sep 22, 2015 8:12:21 PM
+@author: cx
+
+what I do:
+    I simply merge multiple surface form file
+what's my input:
+    file names of surface form file, must be sorted
+        surface form     object   score
+what's my output:
+    a merged file, score added
+
+'''
+
+
+import sys
+import heapq
+
+
+
+if len(sys.argv) < 4:
+    print 'I merge multiple surface form file into one'
+    print 'parameter [inputs]*n + output'
+    sys.exit()
+    
+    
+lIn = [open(InName) for InName in sys.argv[:-1]]
+
+CurrentText = ""
+CurrentScore = 0
+out = open(sys.argv[-1],'w')
+for LineCnt,line in enumerate(heapq.merge(*lIn)):
+    vCol = line.strip().split('\t')
+    text = vCol[:2]
+    score = float(vCol[2])
+    
+    if CurrentText == "":
+        CurrentText = text
+        CurrentScore = score
+        
+    if CurrentText != text:
+        print >> out, CurrentText + '\t%f' %(CurrentScore)
+        CurrentText = text
+        CurrentScore = score
+    else:
+        CurrentScore += score
+        
+print >>out, CurrentText + '\t%f' %(CurrentScore)
+out.close()
+print 'finished'
+        
+    
+
